79036caee46e37a759e3ccc90b8d90d208b20c16
JuanGalarza <groupfuzion@gmail.com>
2022-06-21 11:50:31 -0300

    Update

--- components/first.js
+++ components/first.js
@@ -17,7 +17,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: "50%",
+  width: "90%",
   height: "50%",
   bgcolor: "background.paper",
   borderRadius: "8px",
